3c7ede7eff5250a03128504a4f7a5fa410112347
Ade Septiyadi <ade.septiyadi14@gmail.com>
2020-11-25 10:17:24 +0700

    master

--- app/Http/Controllers/Api/Master/ProductCustomerController.php
+++ app/Http/Controllers/Api/Master/ProductCustomerController.php
@@ -83,7 +83,7 @@ return response()->json($this->productCustomer);
                 'a.product_id',
                 'p.product_code as Kode barang',
                 'p.product_name as Nama barang',
-                'p.uom_code', 'dt.price_idr',
+                'p.uom_code', 'pldt.price_idr',
 		'a.loss_tolerance as Loss tolerance',
 		'a.moq',
 		'a.oem_material_supply_type',
